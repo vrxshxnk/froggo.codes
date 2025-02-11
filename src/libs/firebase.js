6bb974580d5034d49b074e33e648fad751a93bf9
Vrishank <42439148+vrxshxnk@users.noreply.github.com>
2025-02-11 19:22:07 +0530

    knfekjf

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWHHFhAmOpEKhCkAxm4BjV71viTWY0Mo8",
-  authDomain: "froggocodes-ab4ff.firebaseapp.com",
-  projectId: "froggocodes-ab4ff",
-  storageBucket: "froggocodes-ab4ff.firebasestorage.app",
-  messagingSenderId: "899731480476",
-  appId: "1:899731480476:web:2ac6b9737b2a24e64db259",
-  measurementId: "G-ZWKKQRWEGS",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
