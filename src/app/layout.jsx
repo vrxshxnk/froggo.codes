e062211b8d91a62dae1aaf39e977f820cf69ba55
Vrishank <42439148+vrxshxnk@users.noreply.github.com>
2025-02-20 12:51:02 +0530

    minor changes rzp

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,33 @@
 import { AuthProvider } from "@/context/AuthContext";
+import Script from 'next/script';
+import localFont from "next/font/local";
 import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "Python BootCamp",
+  description: "Best way to learn Python. Learn to code in 30 days.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Script
+          src="https://checkout.razorpay.com/v1/checkout.js"
+          strategy="lazyOnload"
+        />
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
